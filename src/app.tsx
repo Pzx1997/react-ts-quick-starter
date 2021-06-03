7621d9a247e6f9f8d0bbddefbd01e975cc225a30
SuperXuan1997@163.com <SuperXuan1997@163.com>
2021-06-03 10:02:42 +0800

    chore: wenpack 配置图片loader

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,10 @@
 import './app.less'
 // import './app.css'
+import image from './assets/img.png'
 
 const root = document.querySelector('#root')
 root.innerHTML = 'hello, webpack!'
+
+const img = document.createElement('img')
+img.src = image
+root.append(img)
