7025d3a68cd3cfcde8ca0c9eb4442f9b2ab5e8f9
SuperXuan1997@163.com <SuperXuan1997@163.com>
2021-06-07 14:01:00 +0800

    chore: 添加包注释

--- scripts/config/webpack.prod.js
+++ scripts/config/webpack.prod.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const { resolve } = require('path')
+const webpack = require('webpack')
 // 查找文件路径
 const glob = require('glob')
 const PurgeCSSPlugin = require('purgecss-webpack-plugin')
@@ -17,6 +18,10 @@ module.exports = merge(common, {
         // 引入样式的tsx文件必须要给到 不然无法解析你没有哪个样式类型！！！
         new PurgeCSSPlugin({
             paths: glob.sync(`${resolve(PROJECT_PATH, './src')}/**/*.{tsx,scss,less,css}`, { nodir: true }),
+        }),
+        new webpack.BannerPlugin({
+            raw: true,
+            banner: '/** @preserve Powered by react-ts-quick-starter (https://github.com/Pzx1997/react-ts-quick-starter) */',
         }),
     ],
 })
