409c95a88997e33e125a4fd25a5d85da26c8e32d
SuperXuan1997@163.com <SuperXuan1997@163.com>
2021-06-07 11:56:53 +0800

    chore: 配置purgecss-webpack-plugin 删除打包后的无用样式代码

--- scripts/config/webpack.prod.js
+++ scripts/config/webpack.prod.js
@@ -1,6 +1,11 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { resolve } = require('path')
+// 查找文件路径
+const glob = require('glob')
+const PurgeCSSPlugin = require('purgecss-webpack-plugin')
+const { PROJECT_PATH } = require('../constants')
 
 module.exports = merge(common, {
     mode: 'production',
@@ -8,5 +13,10 @@ module.exports = merge(common, {
     plugins: [
         // 每次打包清楚上次打包的遗留
         new CleanWebpackPlugin(),
+        // 删除无用的样式代码
+        // 引入样式的tsx文件必须要给到 不然无法解析你没有哪个样式类型！！！
+        new PurgeCSSPlugin({
+            paths: glob.sync(`${resolve(PROJECT_PATH, './src')}/**/*.{tsx,scss,less,css}`, { nodir: true }),
+        }),
     ],
 })
